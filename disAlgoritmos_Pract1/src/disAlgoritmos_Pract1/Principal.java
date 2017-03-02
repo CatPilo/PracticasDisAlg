9189ceaa4337e2282eb519198cec9dbcc99f1ae8
catpilo <catpilo@users.noreply.github.com>
2017-03-02 13:47:31 +0100

    Medimos los tiempos de ejecucion

--- disAlgoritmos_Pract1/src/disAlgoritmos_Pract1/Principal.java
+++ disAlgoritmos_Pract1/src/disAlgoritmos_Pract1/Principal.java
@@ -3,15 +3,24 @@ package disAlgoritmos_Pract1;
 public class Principal {
 
 	public static void main(String[] args) {
-		double[] coeficientes = {-1, 1, 0, 1};
+		double[] coeficientes = {-1, 0, 5, 1};
 		Polinomio ecuacion = new Polinomio(coeficientes);
 		Biseccion biseccion = new Biseccion(ecuacion);
-		double[] intervalo = {0, 1};
-		double error = 0.000000001;
-		double resultado = biseccion.biseccionPilas(intervalo,error);
-		intervalo = biseccion.biseccionRecursiva(intervalo, error);
-		System.out.println(intervalo[0] + " " + intervalo[1]);
-		System.out.println("X=0 para el valor = "+resultado);
+		double[] intervalo = {0, 2};
+		long[] tiemposRecur = {};
+		long[] tiemposPilas = {};
+		double error = 2;
+		for(int i=0; i <= 20 ; i++) {
+		    long startTimeRecur = System.currentTimeMillis();
+		    biseccion.biseccionRecursiva(intervalo, error);
+		    tiemposRecur[i] = System.currentTimeMillis() - startTimeRecur;
+		    long startTimePilas = System.currentTimeMillis();
+		    biseccion.biseccionPilas(intervalo, error);
+		    tiemposPilas[i] = System.currentTimeMillis() - startTimePilas;
+
+		    error *= 0.1;
+		    System.out.println("Recursivo: " + tiemposRecur[i] + " Pilas: " + tiemposPilas[i]);
+		}	        
 	}
 
 }
